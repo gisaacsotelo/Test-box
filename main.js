d2feb4dfc828c58038ba6778a05769472916d71b
gisaacsotelo <gisaac.sotelo@gmail.com>
2022-09-15 09:44:31 +1200

    testing min max sum for an array w high order methods

--- main.js
+++ main.js
@@ -1,55 +1,25 @@
-const tiles = [
-  { id: 1, value: '\u{1F330}', isVisible: false, info: 'chestnut' },
-  { id: 2, value: '\u{1F33D}', isVisible: false, info: 'ear of maze' },
-  // { id: 3, value: '\u{1F33E}', isVisible: false, info: 'ear of rice' },
-  // { id: 4, value: '\u{1F34D}', isVisible: false, info: 'pineapple' },
-  // { id: 5, value: '\u{1F341}', isVisible: false, info: 'maple leaf' },
-  // { id: 6, value: '\u{1F34D}', isVisible: false, info: 'pineapple' },
-  // { id: 7, value: '\u{1F33F}', isVisible: false, info: 'herb' },
-  // { id: 8, value: '\u{1F33D}', isVisible: false, info: 'ear of maze' },
-  // { id: 9, value: '\u{1F33F}', isVisible: false, info: 'herb' },
-  // { id: 10, value: '\u{1F340}', isVisible: false, info: 'four leaf clover' },
-  // { id: 11, value: '\u{1F330}', isVisible: false, info: 'chestnut' },
-  // { id: 12, value: '\u{1F347}', isVisible: false, info: 'grapes' },
-  // { id: 13, value: '\u{1F33E}', isVisible: false, info: 'ear of rice' },
-  // { id: 14, value: '\u{1F347}', isVisible: false, info: 'grapes' },
-  // { id: 15, value: '\u{1F341}', isVisible: false, info: 'maple leaf' },
-  // { id: 16, value: '\u{1F340}', isVisible: false, info: 'four leaf clover' },
-]
+const MyArray = [9,3,5,7,1];
 
-console.log(`tiles before direct push:`, tiles)
-
-tiles.push({ title: 'Bob' })
-
-console.log(`tiles after direct push:`, tiles)
-console.log(`tiles before function push:`, tiles)
-
-add(tiles, { title: 'Henry' })
-
-console.log(`tiles after function push:`, tiles)
-
-let c = [1, 2]
-const d = [1, 2]
-
-console.log(`c before direct push:`, c)
-console.log(`d before direct push:`, d)
-
-c.push(4)
-d.push(4)
-
-console.log(`c after direct push:`, c)
-console.log(`d after direct push:`, d)
-console.log(`c before function push:`, c)
-console.log(`d before function push:`, d)
-
-add(c, 5)
-add(d, 5)
-
-console.log(`c after function push:`, c)
-console.log(`d after function push:`, d)
-
-// ~FUNCTIONS
-
-function add(array, element) {
-  array.push(element)
+function minMax (arr){
+  console.log(arr)
+  let NewArray = arr.sort() // sorts arr min to max
+  console.log(arr)
+  console.log(NewArray)
+  /* 
+  modifies the existing array, no need to call for a new one
+  */
+  let min = arr.slice(0,4)
+  console.log(min)
+  let max = arr.slice(1,5)
+  console.log(max)
+  const minSum = min.reduce((acumulator, currentItem)=> {
+    return acumulator + currentItem
+  },0)
+  console.log(minSum)
+  const maxSum = max.reduce((acumulator, currentItem)=> {
+    return acumulator + currentItem
+  },0)
+  console.log(maxSum)
 }
+
+minMax(MyArray);
